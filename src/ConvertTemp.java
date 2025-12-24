e411fc91241f623c5f67d5ac4f660d015ebe9071
viniciusvog <viniciusgabr89@gmail.com>
2025-12-24 19:52:08 -0300

    Convertendo Celsius para Fahrenheit, quebrado e inteiro

---
+++ src/ConvertTemp.java
@@ -0,0 +1,11 @@
+public class ConvertTemp {
+    public static void main(String[] args) {
+
+        int c = 27;
+        double f = (c * 1.8) + 32;
+        System.out.println("A temperatura convertida de Celsius para Fahrenheit é: " + f);
+
+         int fh = (int) f;
+        System.out.println("A temperatura em Fahrenheit sem número quebrado é: " + fh);
+    }
+}
