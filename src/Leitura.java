239b6458ea62c7472de851b0105a7e1b379e54ca
viniciusvog <viniciusgabr89@gmail.com>
2025-12-25 21:17:48 -0300

    Utilizando o Scanner para entrada de dados

---
+++ src/Leitura.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito: ");
+        String filme = ler.nextLine();
+        System.out.println(filme);
+        System.out.println("QUal o ano de lancamento? ");
+        int anoLancamento = ler.nextInt();
+        System.out.println("Diga a avaliacao para o filme: ");
+        double avaliacao = ler.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoLancamento);
+        System.out.println(avaliacao);
+
+        ler.close();
+    }
+}
