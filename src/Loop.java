602e37ca02aa7c4482383fe6baccb5fd0e6c11eb
viniciusvog <viniciusgabr89@gmail.com>
2025-12-25 21:18:28 -0300

    Utilizando o For

---
+++ src/Loop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Diga a avaliacao para o filme: ");
+            nota = ler.nextDouble();
+            mediaAvaliacao += nota;
+            // += incrementa somando a informacao antes da igualdade com o valor seguinte
+        }
+
+        System.out.println("Media de avaliacoes: " + mediaAvaliacao/3);
+    }
+}
