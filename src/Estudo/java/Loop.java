39f15f3b6fbff153fff6da22fbfd2abf74349c96
viniciusvog <viniciusgabr89@gmail.com>
2025-12-29 15:28:34 -0300

    Organizando os estudos de Java dentro de um package

--- src/Estudo/java/Loop.java
+++ src/Estudo/java/Loop.java
@@ -1,3 +1,5 @@
+package Estudo.java;
+
 import java.util.Scanner;
 
 public class Loop {
--- src/Estudo/java/Main.java
+++ src/Estudo/java/Main.java
@@ -1,3 +1,5 @@
+package Estudo.java;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Esse é o Screen Match!");
